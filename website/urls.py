b7a85bbc2ebd4dc092299103cea11b387a81c383
xudayfi <xudayfiibraahim8@gmail.com>
2023-07-29 15:54:38 +0300

    i am done with login and logout authentications

---
+++ website/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path('logout/', views.logout_user, name="logout")
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    
+    # if login in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'welcome back ' + username + ' you are logged successfully')
+            return redirect('home')
+        else:
+            messages.success(request, 'There was an error please try again')
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('home')

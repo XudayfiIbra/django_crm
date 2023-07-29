459e5cba556c9360065afa44b638c4390b8ecb81
xudayfi <xudayfiibraahim8@gmail.com>
2023-07-29 20:29:32 +0300

    Working with models and admins and it is done

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Record
 
-# Register your models here.
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone_number', 'address', 'city', 'state', 'zipcode', 'created_at')
+
+
+admin.site.register(Record, RecordAdmin)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    created_at = models.TimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150)
+    phone_number = models.IntegerField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=150)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=25)
+     
+    def __str__(self):
+	    return (f"{self.first_name} {self.last_name}")
+    

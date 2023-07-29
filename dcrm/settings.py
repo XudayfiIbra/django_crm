2ba08ca78743eef16d45a7e9dcd492c37b6bd996
xudayfi <xudayfiibraahim8@gmail.com>
2023-07-29 13:51:44 +0300

    initial commit added setting modified

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_crm',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1'
     }
 }
 

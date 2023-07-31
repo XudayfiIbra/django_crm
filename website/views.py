f0e6cf52adbf9cd32bcc5c0f665912cb7dff24c4
xudayfi <xudayfiibraahim8@gmail.com>
2023-07-31 03:21:32 +0300

    ending the viewing records and delete them

--- website/views.py
+++ website/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from .forms import SingUpForm
 from .models import Record
+
+# Home View
+
 def home(request):
     records = Record.objects.all()
     
@@ -22,12 +25,15 @@ def home(request):
     else:
         return render(request, 'home.html', {'records':records})
 
+# Logout View
 
 def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out')
     return redirect('home')
 
+
+# registertions View
 
 def register_user(request):
     if request.method == 'POST':
@@ -47,3 +53,32 @@ def register_user(request):
             'form':form
             })
     return render(request, 'register.html', {'form': form})
+
+
+
+# Customer record View
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look the record
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {
+            'customer_record':customer_record
+        })
+    else:
+        messages.success(request, 'You must logged in to view that page')
+        return redirect('home')
+    
+    
+
+# delete record view
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, 'Record has been successfully deleted')
+        return redirect('home')
+    else:
+        messages.success(request, 'You need to login to delete a record')
+        return redirect('home')
